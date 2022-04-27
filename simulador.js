0debf79e9675105087d9093eefd1b2f945e0c6c6
Felipe Sardans <felipesardans@gmail.com>
2022-04-27 00:01:23 -0300

    actualizacion desafio

--- simulador.js
+++ simulador.js
@@ -1,5 +1,4 @@
 let variable;
-let contador = 0;
 let listapacientes = []
 let persona;
 
@@ -9,65 +8,72 @@ class Pasiente{
         this.Nombre = Nombre
         this.Apellido = Apellido
         this.NumeroAsociado = NumeroAsociado
-        this.TurnoPendiente = false
     }
 
     EliminarTurno(){
         let aux = prompt("Numero de asociado:")
-        for (let posicion in listapacientes) {
-            if (parseInt(posicion.NumeroAsociado) == parseInt(aux)) {
-                listapacientes.splice(parseInt(posicion), 0)
+        for (let cont = 0; cont < listapacientes.length; cont++) {
+            if(parseInt(listapacientes[cont].NumeroAsociado) == parseInt(aux)){
+                listapacientes.splice(parseInt(cont), 1)
             }
         }
+        console.log(listapacientes)
     }
 
+    BuscarDatosPersonales() {
+        let aux = prompt("Numero de asociado:")
+        let datos = listapacientes.find((x) => parseInt(x.NumeroAsociado) == parseInt(aux))
+        console.log(datos)
+        if(datos != null){
+            alert("Nombre: " + datos.Nombre + " Apellido: " + datos.Apellido)
+        }
+        else{
+            alert("El pasiente buscado no existe")
+        }
+    }
 }
 
 do{
     
-    variable = parseInt(prompt("1-Pedir un turno 2-Buscar sus datos 3-Mostrar turno 4-Tiempo estimado de llamada 5-Salir"))
+    variable = parseInt(prompt("1-Pedir un turno 2-Buscar sus datos 3-Dar de baja turno 4-Tiempo estimado de llamada 5-Salir"))
 
     switch(variable){
     
         case 1:
-            persona = new Pasiente(prompt("Ingrese su nombre"),prompt("Ingrese su apellido"),prompt("Ingrese su numero de asociado"));
-            persona.TurnoPendiente = true
+            persona = new Pasiente(prompt("Ingrese su nombre"),prompt("Ingrese su apellido"), ValidarNumeroAsociado()); 
             listapacientes.push(persona)
-            console.log(listapacientes)
-            contador++
         break
         case 2:
             if(listapacientes.length > 0){
-                BuscarDatosPersonales()
+                persona.BuscarDatosPersonales()
             }
             else{
                 alert("No se encontro ningun paciente, porfavor ingrese uno!!!")
             }
         break
         case 3:
-            persona.EliminarTurno()
-        break
-        case 4:
-            if(pass != null && aprobado == true){
-                TiempoEsperaTurno()
+            if(listapacientes.length > 0){
+                persona.EliminarTurno()
             }
             else{
-                alert("Genere su turno primero o validelo")
+                alert("No se encontro ningun paciente, porfavor ingrese uno!!!")
             }
+        break
+        case 4:
+            FacturaHospital()
+        break
     }
 
 }while(variable != 5);
 
-/*function NuevoPaciente() {
-    let persona = new Pasiente(prompt("Ingrese su nombre"),prompt("Ingrese su apellido"),prompt("Ingrese su numero de asociado"));
-    persona.TurnoPendiente = true
-    listapacientes.push(persona)
-    console.log(listapacientes)
-}*/
 
-function BuscarDatosPersonales() {
-    let aux = prompt("Numero de asociado:")
-    let datos = listapacientes.find((x) => parseInt(x.NumeroAsociado) == parseInt(aux))
-    console.log(datos)
-    alert("Nombre: " + datos.Nombre + " Apellido: " + datos.Apellido)
+function ValidarNumeroAsociado(){
+    let numAsociado = prompt("Ingrese numero de asociado")
+    if (listapacientes.some((x) => parseInt(x.NumeroAsociado) == parseInt(numAsociado)))
+    {
+        alert("Ese numero de asociado ya existe, vuelva a ingresarlo")
+        numAsociado = ValidarNumeroAsociado()
+    }
+
+    return numAsociado
 }
