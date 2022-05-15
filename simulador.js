6df1cfd3f98fc85a78ae84788565e355cc8b9288
Felipe Sardans <felipesardans@gmail.com>
2022-05-15 18:44:00 -0300

    segunda entrega

--- simulador.js
+++ simulador.js
@@ -1,5 +1,6 @@
 let listausuarios = []
-let usuario
+let userIniciado
+let cont
 
 class SedesHospitales{
     constructor(id, Localizacion, Areas, Horarios){
@@ -17,36 +18,40 @@ const sede4 = new SedesHospitales(4, "Berazategui", "Pediatraia, Neurologia, Ort
 
 let listasedes = [sede1, sede2, sede3, sede4]
 
-let apartadoSedes = document.getElementById("sedes")
+let apartadoSedes = document.querySelector("#sedes")
+let datosPersonales = document.querySelector("#datos")
 
 class Registro{
 
-    constructor(Nombre, Apellido, NumeroAsociado, Contraseña){
+    constructor(Nombre, Apellido, NumeroAsociado, Contraseña, Turno){
         this.Nombre = Nombre
         this.Apellido = Apellido
         this.NumeroAsociado = NumeroAsociado
         this.Contraseña = Contraseña
-    }
-}
-
-class Usuario{
-    constructor(NumeroAsociado, Contraseña){
-        this.NumeroAsociado = NumeroAsociado
-        this.Contraseña = Contraseña
+        this.Turno = Turno
     }
 }
 
 let pasiente;
-let boton = document.getElementById("aceptarTurno")
-let boton1 = document.getElementById("buscarSedes")
-let boton2 = document.getElementById("pediTurno")
+let boton = document.querySelector("#pediTurno")
+let boton1 = document.querySelector("#buscarSedes")
+let boton2 = document.querySelector("#buscarDatos")
 
 let buttonIniciar = document.querySelector('#formularioInicio')
 let buttonRegistrar = document.querySelector('#formularioRegistro')
+let buttonCerrarSecion = document.querySelector('#IDcerrarsecion')
 
 buttonIniciar.addEventListener('submit', (e) => {
     e.preventDefault()
-    let usuario = new Usuario(document.querySelector("#numAsociado").value, document.querySelector("#IDpassword").value)
+    if (listausuarios.some( (x) => parseInt(document.querySelector("#numAsociado").value) == parseInt(x.NumeroAsociado) && document.querySelector("#IDpassword").value == x.Contraseña)) {
+        userIniciado = listausuarios.find( (x) => parseInt(document.querySelector("#numAsociado").value) == parseInt(x.NumeroAsociado))
+        alert("Bienvenido, " + userIniciado.Nombre)
+        buttonIniciar.reset()
+        formState()
+    }
+    else{
+        alert("Numero de asociado o contraseña incorrecta")
+    }
     if (usuario != null) {
         buttonIniciar.reset()
         formState()
@@ -55,14 +60,46 @@ buttonIniciar.addEventListener('submit', (e) => {
 
 buttonRegistrar.addEventListener('submit', (e) => {
     e.preventDefault()
-    let usuarioRegistrado = new Registro(document.querySelector("#IDnombre").value, document.querySelector("#IDapellido").value, document.querySelector("#IDnumAsociado").value, document.querySelector("#IDpasswordR").value)
-    alert(usuarioRegistrado.NumeroAsociado)
+    let usuarioRegistrado = new Registro(document.querySelector("#IDnombre").value, document.querySelector("#IDapellido").value, document.querySelector("#IDnumAsociado").value, document.querySelector("#IDpasswordR").value, "NO")
+    listausuarios.push(usuarioRegistrado)
+    buttonRegistrar.reset()
+    alert('Te as registrado con exito')
+})
+
+buttonCerrarSecion.addEventListener('click', () =>{
+    if (userIniciado != null) {
+        userIniciado = null
+        formState()
+        if (cont == 1) {
+            for (let index = 1; index < 5; index++) {
+                let element = document.querySelector('#sede' + index)
+                apartadoSedes.removeChild(element)
+            }
+        }
+        let elementDatos = document.querySelector('#plantilla')
+        datosPersonales.removeChild(elementDatos)
+        document.getElementById("buscarSedes").disabled = false
+    }
+    else{
+        alert("No hay una secion iniciada anteriormente")
+    }
 })
 
 function formState() {
-    document.querySelector("#IDpassword").disabled = true
-    document.querySelector("#numAsociado").disabled = true
-    document.querySelector("#aceptarTurno").disabled = true
+    if (userIniciado != null) {
+        document.querySelector("#IDpassword").disabled = true
+        document.querySelector("#numAsociado").disabled = true
+        document.querySelector("#aceptarTurno").disabled = true
+        document.querySelector("#pediTurno").disabled = false
+        document.querySelector("#buscarDatos").disabled = false
+    }
+    else{
+        document.querySelector("#IDpassword").disabled = false
+        document.querySelector("#numAsociado").disabled = false
+        document.querySelector("#aceptarTurno").disabled = false
+        document.querySelector("#pediTurno").disabled = true
+        document.querySelector("#buscarDatos").disabled = true
+    }
 }
 
 /*function buttonState()
@@ -93,7 +130,7 @@ boton.addEventListener("click", (e) => {
     Extras()
     console.log(listapacientes)
     }
-})
+})*/
 
 
 boton1.addEventListener('click', () =>{
@@ -106,7 +143,32 @@ boton1.addEventListener('click', () =>{
         </div>
         `
     })
+    cont = 1
     boton1.disabled = true
+})
+
+boton.addEventListener('click', () => {
+    if (userIniciado.Turno == "NO") {
+        listausuarios.forEach(user => {
+            if (user.numAsociado == userIniciado.numAsociado) {
+                user.Turno = "SI"
+                alert("El turno fue validado")
+            }
+        })
+    }
+    else{
+        alert("El pasiente cuenta con un turno vigente")
+    }
+})
+
+boton2.addEventListener('click', () =>{
+    datosPersonales.innerHTML = `
+    <div id="plantilla">
+        <h2>${userIniciado.Nombre} ${userIniciado.Apellido}</h2>
+        <p>Numero de asociado: ${userIniciado.NumeroAsociado}</p>
+        <p>Turno pendiente: ${userIniciado.Turno}</p>
+    </div>
+    `
 })
 
 function ValidarNumeroAsociado(){
@@ -117,4 +179,4 @@ function ValidarNumeroAsociado(){
     }
 
     return numAsociado
-}*/
+}
